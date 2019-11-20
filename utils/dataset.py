90e681b85ca7c770845445d8baf2da2441910de3
tallua <talluay@gmail.com>
2019-11-20 19:58:39 +0900

    change data augmentation algorithm

--- utils/dataset.py
+++ utils/dataset.py
@@ -41,6 +41,11 @@ class PickingProcess(object):
         output['title'] = meta['title'] + self.postfix
         output['background_path'] = meta['background_path']
         output['faces'] = [meta['faces'][person]['emote']['bbox'] for person in meta['faces']]
+        try:
+            output['background'] = PIL.Image.open(params['in_dir'] + output['background_path'])
+        except IOError:
+            print('cannot load image :', params['in_dir'] + output['background_path'])
+            return None
 
         return output
 
@@ -53,20 +58,18 @@ class ResizingProcess(object):
         if meta is None:
             return None
 
-        try :
-            meta['background'] = PIL.Image.open(params['in_dir'] + meta['background_path'])
-            width, height = meta['background'].size
-            width_rate = self.size[0] / float(width)
-            height_rate = self.size[1] / float(height)
-
-            meta['faces'] = [[bbox[0] * width_rate, bbox[1] * height_rate, 
-                bbox[2] * width_rate, bbox[3] * height_rate] for bbox in meta['faces']]
-                
-            meta['background'] = meta['background'].resize(self.size, self.opt)
-            return meta
-        except IOError:
-            print('cannot resize image :', params['in_dir'] + meta['background_path'])
-            return None
+        width, height = meta['background'].size
+        
+        width_rate = self.size[0] / float(width)
+        height_rate = self.size[1] / float(height)
+        min_rate = min(width_rate, height_rate)
+        
+        old_image = meta['background'].resize((int(width * min_rate), int(height * min_rate)), self.opt)
+        new_image = PIL.Image.new("RGB", (self.size[0], self.size[1]))
+        new_image.paste(old_image, old_image.getbbox())
+        
+        meta['faces'] = [[val * min_rate for val in bbox] for bbox in meta['faces']]
+        meta['background'] = new_image
 
 class SavingProcess(object):
     def __call__(self, params, meta):
@@ -104,84 +107,79 @@ class CropResizingProcess(object):
         if meta is None:
             return None
 
-        try :
-            orig_img = PIL.Image.open(params['in_dir'] + meta['background_path'])
-            orig_faces = meta['faces']
-            orig_w, orig_h = orig_img.size
+        orig_faces = meta['faces']
+        orig_w, orig_h = meta['background'].size
             
-            min_w = self.size[0] // 2
-            if self.size[0] > orig_w :
-                min_w = orig_w
-            max_w = orig_w
+        min_w = self.size[0] // 2
+        if self.size[0] > orig_w :
+            min_w = orig_w
+        max_w = orig_w
+               
+        min_h = self.size[1] // 2
+        if self.size[1] > orig_h :
+            min_h = orig_h
+        max_h = orig_h
                 
-            min_h = self.size[1] // 2
-            if self.size[1] > orig_h :
-                min_h = orig_h
-            max_h = orig_h
-            
-            
-            if min_w == max_w or min_h == max_h:
-                return None
-            
-            for _ in range(0, 10) :
-                
-
-                new_w = random.randrange(min_w, max_w, 1)
-                new_h = random.randrange(min_h, max_h, 1)
-                new_x1 = random.randrange(0, max_w - new_w, 1)
-                new_x2 = new_x1 + new_w
-                new_y1 = random.randrange(0, max_h - new_h, 1)
-                new_y2 = new_y1 + new_h
-            
-                if abs((orig_w / orig_h) - (new_w / new_h)) < 0.5:
-                    continue
-            
-                collide = False
-                new_faces = []
-                for face in orig_faces:
-                    if (face[0] - new_x1) * (face[2] - new_x1) < 0:
-                        collide = True
-                        break
-                    if (face[0] - new_x2) * (face[2] - new_x2) < 0:
-                        collide = True
-                        break
-                    if (face[1] - new_y1) * (face[3] - new_y1) < 0:
-                        collide = True
-                        break
-                    if (face[1] - new_y2) * (face[3] - new_y2) < 0:
-                        collide = True
-                        break
-                
-                    new_face = [face[0] - new_x1, face[1] - new_y1, face[2] - new_x1, face[3] - new_y1]
-                
-                    if new_face[0] < 0 or new_face[1] < 0 or new_face[2] < 0 or new_face[3] < 0:
-                        continue
-                    
-                    if new_face[0] > new_w or new_face[1] > new_h or new_face[2] > new_w or new_face[3] > new_h:
-                        continue
-                    
-                    new_faces.append(new_face)
-                    
-                if collide is True :
-                    continue
-                    
-                if len(new_faces) == 0:
-                    continue
-            
-                width_rate = self.size[0] / float(new_w)
-                height_rate = self.size[1] / float(new_h)
-            
-                new_faces = [[bbox[0] * width_rate, bbox[1] * height_rate, 
-                    bbox[2] * width_rate, bbox[3] * height_rate] for bbox in new_faces]
-            
-                meta['faces'] = new_faces
-                meta['background'] = orig_img.crop((new_x1, new_y1, new_x2, new_y2)).resize(self.size, self.opt)
-            
-                return meta
+        if min_w == max_w or min_h == max_h:
             return None
-        except IOError:
-            print('cannot resize image :', params['in_dir'] + meta['background_path'])
-            return None
+            
+        for _ in range(0, 10) :
+            new_w = random.randrange(min_w, max_w, 1)
+            new_h = random.randrange(min_h, max_h, 1)
+            new_x1 = random.randrange(0, max_w - new_w, 1)
+            new_x2 = new_x1 + new_w
+            new_y1 = random.randrange(0, max_h - new_h, 1)
+            new_y2 = new_y1 + new_h
+            
+            if abs((orig_w / orig_h) - (new_w / new_h)) < 0.5:
+                continue
+            
+            collide = False
+            new_faces = []
+            for face in orig_faces:
+                if (face[0] - new_x1) * (face[2] - new_x1) < 0:
+                    collide = True
+                    break
+                if (face[0] - new_x2) * (face[2] - new_x2) < 0:
+                    collide = True
+                    break
+                if (face[1] - new_y1) * (face[3] - new_y1) < 0:
+                    collide = True
+                    break
+                if (face[1] - new_y2) * (face[3] - new_y2) < 0:
+                    collide = True
+                    break
+                
+                new_face = [face[0] - new_x1, face[1] - new_y1, face[2] - new_x1, face[3] - new_y1]
+                
+                if new_face[0] < 0 or new_face[1] < 0 or new_face[2] < 0 or new_face[3] < 0:
+                    continue
+                    
+                if new_face[0] > new_w or new_face[1] > new_h or new_face[2] > new_w or new_face[3] > new_h:
+                    continue
+                    
+                new_faces.append(new_face)
+                    
+            if collide is True :
+                continue
+                    
+            if len(new_faces) == 0:
+                continue
+            
+            width_rate = self.size[0] / float(new_w)
+            height_rate = self.size[1] / float(new_h)
+            min_rate = min(width_rate, height_rate)
+            
+            old_image = meta['background'].crop((new_x1, new_y1, new_x2, new_y2)).resize((int(new_w * min_rate), int(new_h * min_rate)), self.opt)
+            new_image = PIL.Image.new("RGB", (self.size[0], self.size[1]))
+            new_image.paste(old_image, old_image.getbbox())
+        
+        
+            meta['faces'] = [[val * min_rate for val in bbox] for bbox in new_faces]
+            meta['background'] = new_image
+            
+            return meta
+        return None
         
         
 class DetectionFolder(Dataset):
